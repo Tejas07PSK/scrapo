a6e3830663883b7dbaaf6f5be6a8988bda5146f5
Palash Sarkar <palashsarkar0007@gmail.com>
2019-01-03 21:12:14 +0530

    Added 'upload.js' script for 'image_cloud' module !!

--- image_cloud/upload.js
+++ image_cloud/upload.js
@@ -10,6 +10,6 @@ const cc = require('./cloudinaryConfig');
 const cloud = require('cloudinary');
 cloud.config(cc.config);
 
-//testing file upload to cloudinary
+//uploading processed image;
 
 cloud.v2.uploader.upload("../imgcache/temp.jpg", function (error, result) { console.log(result, error); });
