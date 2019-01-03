bcbb05ddc3898d2859db6b8e47e5064407cbdce9
Palash Sarkar <palashsarkar0007@gmail.com>
2019-01-03 16:43:54 +0530

    Added 'compressandfilter.js' script for 'filter_compress_image' module !!

---
+++ filter_compress_images/compressandfilter.js
@@ -0,0 +1,21 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 03 Jan, 2019, 3:44 PM,
+        @File-Name : compressandfilter.js.js
+
+ */
+
+const jimp = require('jimp');
+
+module.exports.compressAndFilter = async function (url) {
+
+    let img_buffer = null;
+    await jimp.read(url).then(
+
+        (imgObj) => { img_buffer = (imgObj.resize(256, 256).quality(60).grayscale())['bitmap'].data; }
+
+    ).catch((err) => { console.log("Error !! Could not read image from 'URL' !! \n " + err); });
+    return (img_buffer);
+
+};
