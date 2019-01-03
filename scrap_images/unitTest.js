39c6e6304e29a3039c17d8abf8ab1beed78d7005
Palash Sarkar <palashsarkar0007@gmail.com>
2019-01-03 12:43:47 +0530

    Added 'unitTest.js' for 'scrap_images' module !!

---
+++ scrap_images/unitTest.js
@@ -0,0 +1,11 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 03 Jan, 2019, 12:41 PM,
+        @File-Name : unitTest.js
+
+ */
+
+const scrapper = require("./scrapper");
+
+scrapper.scrape("dogs");
