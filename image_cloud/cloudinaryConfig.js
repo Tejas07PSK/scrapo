e3dcc34e5b164991b7c1861fd93ac01f5d708a15
Palash Sarkar <palashsarkar0007@gmail.com>
2019-01-03 21:08:19 +0530

    Added 'coudinaryConfig.js' script for 'image_cloud' module !!

---
+++ image_cloud/cloudinaryConfig.js
@@ -0,0 +1,15 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 03 Jan, 2019, 7:17 PM,
+        @File-Name : cloudinaryConfig.js
+
+ */
+
+module.exports.config = {
+
+    "cloud_name" : 'dczwplg2j',
+    "api_key" : '123951921577524',
+    "api_secret" : 'OkYRfz9-y5BQghw8qrXj4sVpd6I'
+
+};
