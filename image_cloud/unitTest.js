950c94b9ba001e7e69be0b6e04c0ba62bed099a4
Palash Sarkar <palashsarkar0007@gmail.com>
2019-01-04 18:24:38 +0530

    Updated 'unitTest.js' script for 'image_cloud' module !!

--- image_cloud/unitTest.js
+++ image_cloud/unitTest.js
@@ -11,12 +11,13 @@
 const prep = require('../filter_compress_images/compressandfilter');
 const url = "https://cdn-images-1.medium.com/max/800/1*4AApxPkF4yXzY0jF0yHgkA.jpeg";
 const img_cld = require('./upload');
+const filename = '../public/imgcache/temptest0.jpg';
 
 // testing upload to cloudinary.
 
-prep.compressAndFilter(url).then(() => {
+prep.compressAndFilter(url, filename).then((fln) => {
 
-        img_cld.push().then((res) => { console.log("Secure url of uploaded image : " + res); })
+        img_cld.push(fln).then((res) => { console.log("Secure url of uploaded image : " + res); })
         .catch((err) => { console.log("Could not upload image to cloudinary !! Error !!"); console.error(err) });
 
     }, (err) => { console.log(err); });
