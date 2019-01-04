a7c621f2479a5302bd0aae519df61c476af9e5d2
Palash Sarkar <palashsarkar0007@gmail.com>
2019-01-04 16:39:02 +0530

    Updated 'unitTest.js' script for 'filter_compress_images' module !!

--- filter_compress_images/unitTest.js
+++ filter_compress_images/unitTest.js
@@ -10,7 +10,8 @@
 
 const prep = require('./compressandfilter');
 const url = "https://cdn-images-1.medium.com/max/800/1*4AApxPkF4yXzY0jF0yHgkA.jpeg";
+const filename = '../public/imagecache/temptest0.jpg';
 
 // testing image-compression and grayscale-filter.
 
-prep.compressAndFilter(url).then(() => { console.log("Check \'imgcache\' folder, image processing complete."); }, (err) => { console.log(err); });
+prep.compressAndFilter(url, filename).then((res) => { console.log(`File name - ${res}`); console.log("Check \'imgcache\' folder, image processing complete."); }, (err) => { console.log(err); });
