0a326bab0d53a9212c81cf6a62b53ba15444c3bd
Palash Sarkar <palashsarkar0007@gmail.com>
2019-01-05 03:06:38 +0530

    Added 'app.js' script !!

---
+++ app.js
@@ -0,0 +1,55 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 04 Dec, 2018, 12:46 PM,
+        @File-Name : app.js
+
+ */
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const keysRouter = require('./routes/keys');
+const imagesRouter = require('./routes/images');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/keys', keysRouter);
+app.use('/images', imagesRouter);
+
+// catch 404 and forward to error handler
+
+app.use(function(req, res, next) { next(createError(404)); });
+
+// error handler
+
+app.use(function(err, req, res, next) {
+
+  // set locals, only providing error in development
+
+  res.locals.message = err.message;
+  res.locals.error = ((req.app.get('env') === 'development') ? err : {});
+
+  // render the error page
+
+  res.status(err.status || 500);
+  res.render('error');
+
+});
+
+module.exports = app;
