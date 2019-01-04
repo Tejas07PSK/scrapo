521ad677757c348357e73a0a68a274ed18d5d0f5
Palash Sarkar <palashsarkar0007@gmail.com>
2019-01-04 18:27:03 +0530

    Updated 'upload.js' script for 'image_cloud' module !!

--- image_cloud/upload.js
+++ image_cloud/upload.js
@@ -15,14 +15,14 @@ cloud.config(cc.config);
 
 // uploading processed image.
 
-module.exports.push = async function () {
+module.exports.push = async function (fln) {
 
-    let img_url = undefined; //will store the secure_url of the image stored in the cloud
-    await cloud.v2.uploader.upload("../imgcache/temp.jpg", function (error, result) {
+    let img_url = undefined; //will store the secure_url of the image stored in cloud
+    await cloud.v2.uploader.upload(fln, {"folder" : 'scrapo'}, function (error, result) {
 
         if (error) { console.log(error); return; }
         img_url = result['secure_url'];
-        try { fs.unlinkSync("../imgcache/temp.jpg"); } catch(err) { console.log("File not removed !! Error !!"); console.error(err); }
+        try { fs.unlinkSync(fln); } catch(err) { console.log("File not removed !! Error !!"); console.error(err); }
 
     });
     return (img_url);
