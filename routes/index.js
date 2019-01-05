17f260fbc3c535fddbf9cbdd6358528358ee9594
Palash Sarkar <palashsarkar0007@gmail.com>
2019-01-05 22:23:32 +0530

    Updated 'index.js' routing script under 'routes' module !!

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,6 @@
 
 "use strict";
 
-/*const scrapper = require('../scrap_images/scrapper');
-const img_cld = require('../image_cloud/upload');
-const prep = require('../filter_compress_images/compressandfilter');
-const crud = require('../models/crud');*/
 const spawn = require('child_process').spawn;
 const router = (require('express')).Router();
 
@@ -41,117 +37,7 @@ router.post('/', function (req, res) {
         "utf8", function () { console.log("Http conversation ended successfully !!"); }
 
     );
-    /*crud.getDocFromKey(key).then((lnks) => {
 
-        if (lnks === undefined) {
-
-            res.status(500);
-            res.end("Internal Error !! (http - 500)", "utf-8", function () { console.log("Http conversation ended successfully !!"); });
-
-        }
-        else if (lnks === null) {
-
-            (scrapper.scrape(key)).then((glimgurls) => {
-
-                pt1(glimgurls, key).then((flns) => {
-
-                    pt2(flns, key).then((upimgurls) => {
-
-                        if (upimgurls.length === 0) { res.status(500); res.end("Internal Error !! (http - 500)", "utf-8", function () { console.log("Http conversation ended successfully !!"); }); return; }
-                        crud.createDoc({ "key" : key, "links" : upimgurls }).then((flag) => {
-
-                            if (flag) {
-
-                                res.status(200);
-                                res.end("Successfully added \'key\' - " + key + " with it's \'images\' to database !! (http - 200)", "utf-8", function () { console.log("Http conversation ended successfully !!"); });
-
-                            }
-                            else {
-
-                                res.status(500);
-                                res.end("Internal Error !! (http - 500)", "utf-8", function () { console.log("Http conversation ended successfully !!"); });
-
-                            }
-
-                        }).catch((err3) => {
-
-                            console.error(err3);
-                            res.status(500);
-                            res.end("Internal Error !! (http - 500)", "utf-8", function () { console.log("Http conversation ended successfully !!"); });
-
-                        });
-
-                    }).catch((err2) => {
-
-                        console.error(err2);
-                        res.status(500);
-                        res.end("Internal Error !! (http - 500)", "utf-8", function () { console.log("Http conversation ended successfully !!"); });
-
-                    });
-
-                }).catch((err1) => {
-
-                    console.error(err1);
-                    res.status(500);
-                    res.end("Internal Error !! (http - 500)", "utf-8", function () { console.log("Http conversation ended successfully !!"); });
-
-                });
-
-            }).catch((err0) => {
-
-                console.error(err0);
-                res.status(500);
-                res.end("Internal Error !! (http - 500)", "utf-8", function () { console.log("Http conversation ended successfully !!"); });
-
-            });
-
-        }
-        else {
-
-            res.status(200);
-            res.end("\'key\' - " + key + " with it's \'images\' already exists in database !! (http - 200)", "utf-8", function () { console.log("Http conversation ended successfully !!"); });
-
-        }
-
-    }).catch((fault) => {
-
-        console.error(fault);
-        res.status(500);
-        res.end("Internal Error !! (http - 500)", "utf-8", function () { console.log("Http conversation ended successfully !!"); });
-
-    });*/
 });
-
-/*async function pt1(glimgurls, key) {
-
-    let flns = [];
-    for (let i = 0; i < glimgurls.length; i += 1) {
-
-        await prep.compressAndFilter(glimgurls[i], ("public/imgcache/temp" + key + i + ".jpg")).then(
-
-            (fln) => { if (fln !== undefined) { flns.push(fln); } }
-
-        ).catch((err) => { console.log(err); });
-
-    }
-    return (flns);
-
-}
-
-async function pt2(flns, key) {
-
-    let sec_urls = [];
-    for (let i = 0; i < flns.length; i += 1) {
-
-        await img_cld.send(flns[i], key).then((img_url) => {
-
-            sec_urls.push(img_url);
-
-        }).catch((err) => { console.log(err); })
-
-    }
-    return (sec_urls);
-
-}*/
 
 module.exports = router;
