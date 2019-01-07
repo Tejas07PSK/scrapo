a65a6f6729caea24a4a68aa70e383d9bfc6e0702
Palash Sarkar <palashsarkar0007@gmail.com>
2019-01-08 02:33:25 +0530

    Added 'scraper.js' script under 'scrape_images' module !!

---
+++ scrape_images/scraper.js
@@ -0,0 +1,34 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 03 Jan, 2019, 12:16 PM,
+        @File-Name : scraper.js
+
+ */
+
+"use strict";
+
+const cheerio = require('cheerio');
+const google = require('./googleimages');
+let $ = undefined;
+
+// start scraping first 15 images from google.
+
+module.exports.scrape = async function (key){
+
+    let img_urls = [], temp = undefined;
+    await google.getImages(key).then(
+
+        (data) => {
+
+            $ = cheerio.load(data);
+            temp = ($("div#rg_s div.rg_bx div.rg_meta").toArray()).slice(0, 15);
+            temp.forEach((dom_obj) => { img_urls.push(JSON.parse(dom_obj.children[0].data)['tu']); });
+
+        },
+        (err) => { console.log('Scraping of images failed !!'); console.error(err); }
+
+    );
+    return(img_urls);
+
+};
